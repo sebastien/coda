4bfefd39c70c70ff05fdae551627c7fce19b36ed
Sébastien Pierre <sebastien.pierre@gmail.com>
2023-01-06 20:31:07 +1300

    [New] tests: Grammar testing

---
+++ tests/utils-grammar.py
@@ -0,0 +1,84 @@
+from coda.utils.grammar import grammar
+from harness import test, same, fail, out, hlo
+
+hlo("Exercises the grammar rules for state machines")
+
+
+def check(rules: dict[str, str], input: list[str], expected: list[list[str]]):
+    """Checkes that the input produces the output with the given rules"""
+    i: int = 0
+    parser = grammar(rules)
+
+    for match in parser.run(input):
+        name = match.name
+        if not same([match.name] + input[match.start : match.end], expected[i]):
+            out(f" …  {parser}")
+        i += 1
+    if i < len(expected):
+        fail(f"Expected {len(expected)} got {i}, missing: {expected[i:]}")
+        out(f" …  {parser}")
+
+
+if True:
+    with test("Grammar ONE combinator"):
+        check(
+            {"M": "T"},
+            ["T"],
+            [
+                ["M", "T"],
+            ],
+        )
+        check(
+            {"M": "T"},
+            ["T", "T"],
+            [
+                ["M", "T"],
+                ["M", "T"],
+            ],
+        )
+
+    with test("Grammar OPTIONALLY ONE combinator"):
+        check(
+            {"M": "T?"},
+            ["T"],
+            [
+                ["M", "T"],
+            ],
+        )
+        # The match should be empty as T matches anything.
+        check(
+            {"M": "T?"},
+            ["A"],
+            [
+                ["M"],
+            ],
+        )
+
+    with test("Grammar MANY combinator"):
+        for t in (_.split() for _ in ["T", "T T", "T T T", "T T T T T T T"]):
+            check(
+                {"M": "T+"},
+                t,
+                [
+                    ["M"] + t,
+                ],
+            )
+
+    with test("Grammar OPTIONALLY MANY combinator"):
+        check(
+            {"M": "T*"},
+            ["A"],
+            [
+                ["M"],
+            ],
+        )
+        for t in (_.split() for _ in ["T", "T T", "T T T", "T T T T T T T"]):
+            check(
+                {"M": "T+"},
+                t,
+                [
+                    ["M"] + t,
+                ],
+            )
+
+# EOF
