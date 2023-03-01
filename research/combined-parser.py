6895f3b4ce397fbbd856eb48fdb8feaf2db4ae80
Sébastien Pierre <sebastien.pierre@gmail.com>
2023-03-02 10:44:27 +1300

    [WIP] parser: extracting stuff as texto

--- research/combined-parser.py
+++ research/combined-parser.py
@@ -1,6 +1,7 @@
 from typing import Iterator, Optional, NamedTuple
 from dataclasses import dataclass
-from pathlib import Path
+from html import escape
+import texto
 import re
 
 # --
@@ -26,6 +27,7 @@ class Fragment(NamedTuple):
 @dataclass
 class Block:
     type: str
+    meta: Optional[str]
     fragment: Fragment
     lines: list[str]
 
@@ -45,65 +47,82 @@ class BlockParser:
         block_type: Optional[str] = None
         block_start: int = 0
         block_lines: list[str] = []
+        block_meta: Optional[str] = None
+        # NOTE: This should be refactored, it's too repetitive
         for line in lines:
             if match := RE_CODA_BLOCK.match(line):
                 if block_type:
-                    yield Block(block_type, Fragment(block_start, offset), block_lines)
-                meta = match.group("meta").split()
+                    yield Block(
+                        block_type,
+                        block_meta,
+                        Fragment(block_start, offset),
+                        block_lines,
+                    )
                 block_start = offset
-                block_type = meta[0] if meta else "text"
+                block_type = "text"
                 block_lines = [line]
+                block_meta = match.group("meta").strip()
             elif RE_COMMENT.match(line):
-                if block_type in ("coda", "comment"):
+                if block_type in ("text", "comment"):
                     block_lines.append(line)
                 else:
                     if block_type:
                         yield Block(
-                            block_type, Fragment(block_start, offset), block_lines
+                            block_type,
+                            block_meta,
+                            Fragment(block_start, offset),
+                            block_lines,
                         )
                     block_type = "comment"
                     block_start = offset
                     block_lines = [line]
+                    block_meta = None
             else:
                 if block_type == "code":
                     block_lines.append(line)
                 else:
                     if block_type:
                         yield Block(
-                            block_type, Fragment(block_start, offset), block_lines
+                            block_type,
+                            block_meta,
+                            Fragment(block_start, offset),
+                            block_lines,
                         )
                     block_type = "code"
                     block_start = offset
                     block_lines = [line]
+                    block_meta = None
 
             offset += len(line)
         if block_type and block_lines:
-            yield Block(block_type, Fragment(block_start, offset), block_lines)
+            yield Block(
+                block_type, block_meta, Fragment(block_start, offset), block_lines
+            )
 
 
 class BlockFormatter:
     @staticmethod
     def HTML(blocks: Iterator[Block]):
-        yield "<html><body>"
         for block in blocks:
             match block.type:
                 case "comment":
-                    yield "<pre style='color:green;'>"
-                    yield from (_ for _ in block.lines)
+                    yield f"<pre class={block.type} style='color:green;'>"
+                    yield from (escape(_) for _ in block.lines)
                     yield "</pre>"
                 case "code":
-                    yield "<pre style='color:grey;'>"
-                    yield from (_ for _ in block.lines)
+                    yield f"<pre class={block.type} style='color:grey;'>"
+                    yield from (escape(_) for _ in block.lines)
                     yield "</pre>"
-                case "coda":
-                    yield "<em style='color:blue;'>"
-                    yield from (_ for _ in block.lines)
+                case "text":
+                    yield f"<em class={block.type} style='color:blue;'>"
+                    text = "\n".join(_.strip().lstrip("# ") for _ in block.lines[1:])
+                    yield texto.render(texto.parse(text))
                     yield "</em>"
                 case _:
-                    yield "<pre>"
-                    yield from (_ for _ in block.lines)
+                    yield f"<pre class={block.type}>"
+                    yield from (escape(_) for _ in block.lines)
                     yield "</pre>"
-        yield "</body>/<html>"
+        yield "</body><html>"
 
 
 if __name__ == "__main__":
